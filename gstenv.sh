e8fc9f8cf712e1e0f8ea9cfb1d7e3a29c24a2d8e
Naveen Karuthedath <naveen.karuthedath@gmail.com>
2016-10-07 15:31:33 +0530

    Initial basic RTSP Server

---
+++ gstenv.sh
@@ -0,0 +1,5 @@
+PFX=/home/vvdn/gst-1.2.3/runtime
+export PATH=$PATH:$PFX/bin
+export LD_RUN_PATH=$LD_RUN_PATH:$PFX/lib
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$PFX/lib/pkgconfig
+export GST_PLUGIN_PATH=$PFX/lib/gstreamer-1.0
